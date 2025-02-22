f793d85726724c1dd5ac92a57d086384abc83985
zanthrash <zanthrash@gmail.com>
2025-02-22 11:48:03 -0600

    adding vim bindins to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,24 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -g fish_key_bindings fish_vi_key_bindings
+# Emulates vim's cursor shape behavior
+# Set the normal and visual mode cursors to a block
+set fish_cursor_default block blink
+# Set the insert mode cursor to a line
+set fish_cursor_insert line blink
+# Set the replace mode cursors to an underscore
+set fish_cursor_replace_one underscore blink
+set fish_cursor_replace underscore blink
+# Set the external cursor to a line. The external cursor appears when a command is started.
+# The cursor shape takes the value of fish_cursor_default when fish_cursor_external is not specified.
+set fish_cursor_external line
+# The following variable can be used to configure cursor shape in
+# visual mode, but due to fish_cursor_default, is redundant here
+set fish_cursor_visual block
+
+set fish_vi_force_cursor 1
+
 starship init fish | source
 
 alias vim="nvim"
